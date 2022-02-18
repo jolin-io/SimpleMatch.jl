602fb2f74964e92082fc5f882d03d60f54bbd856
Stephan Sahm <stephan.sahm@gmx.de>
2022-02-18 17:14:53 +0100

    implemented anonymous functions
    but don't find it good, better to stay with the named one because it allows for recursive calls

--- src/SimpleMatch.jl
+++ src/SimpleMatch.jl
@@ -25,16 +25,36 @@ We need to define this as macro, as it turns out to be relatively difficult
 macro match(define_func!, args...)
   @assert define_func!.head == :(->)  "expecting anonymous function as first argument"
   @assert define_func!.args[1].head == :tuple  "expecting anonymous function as first argument"
-  @assert length(define_func!.args[1].args) == 1  "expecting define_func! to take exactly 1 argument, found $(length(define_func!.args[1].args))"
 
-  function_name = define_func!.args[1].args[1]  # single argument of function
-  function_definition = define_func!.args[2]  # function body
+  do_args = define_func!.args[1].args  # @match do args
+  do_body = define_func!.args[2]  # @match do body
+  
+  if length(do_args) == 0
+    @gensym name
+
+    # name anonymous functions
+    for expr in do_body.args
+      if isa(expr, Expr) && expr.head ∈ (:function, :->, :(=))
+        @assert expr.args[1].head ∈ (:tuple, :...) "expecting anonymous functions within body of @match, found function named '$(expr.args[1].args[1])'"
+        # make it to a named function
+        expr.head = :function
+        func_args = expr.args[1].head == :... ? [expr.args[1]] : expr.args[1].args
+        expr.args[1] = Expr(:call, name, func_args...)
+      end
+    end
+
+  elseif length(do_args) == 1
+    name = do_args[1]
+
+  else
+    throw(AssertionError("expecting do-block to take 0 or 1 argument, found $(length(define_func!.args[1].args))"))
+  end
 
   esc(quote
     let
-      function $function_name end
-      $function_definition
-      $function_name($(args...))
+      function $name end
+      $do_body
+      $name($(args...))
     end
   end)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,23 @@ using Test
 @test isempty(detect_ambiguities(SimpleMatch))
 
 a = 1
+b = "2.0"
+matched = @match(a, b) do
+  (::Int, ::AbstractFloat) -> b + 3
+  function (args...) 
+    args[1] + 9
+  end
+end
+@test matched == 10
+
+
+
+a = 1
 b = 2.0
 matched = @match(a, b) do f
   f(::Int, ::AbstractFloat) = b + 3
-  f(args...) = a + 9
+  function f(args...)
+    args[1] + 9
+  end
 end
 @test matched == 5.0
